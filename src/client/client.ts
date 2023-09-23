1e39d2e1fb396d48a19dc77ee7fda833fec21efd
Shovan Swain <shovanswain@gmail.com>
2023-09-23 18:43:55 +0530

    FOV nice

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,12 +6,18 @@ import { GUI } from 'dat.gui'
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(
-    190, 
-    window.innerWidth / window.innerHeight, 
-    0.01, 
-    10
-    )
-camera.position.z = 1.1
+    250,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+
+// const camera = new THREE.OrthographicCamera(
+//     -10,10,10,-10,0.001,100
+// )
+camera.position.x = 5
+camera.position.y = 0
+camera.position.z = 5
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth,window.innerHeight)
@@ -40,27 +46,77 @@ const stats = new Stats()
 document.body.appendChild(stats.dom)
 
 const gui = new GUI()
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 10)
-cameraFolder.add(camera, 'far', 1, 30)
-cameraFolder.add(camera, 'near', 0, 10)
-cameraFolder.add(camera, 'focus', 0, 10)
-cameraFolder.add(camera, 'fov', 20, 200)
-cameraFolder.open()
-console.log(camera)
 
+const sphereData = {
+    radius: 1,
+    widthSegments: 8,
+    heightSegments: 6,
+    phiStart: 0,
+    phiLength: Math.PI * 2,
+    thetaStart: 0,
+    thetaLength: Math.PI,
+}
+const sphereFolder = gui.addFolder('Sphere')
+const spherePropertiesFolder = sphereFolder.addFolder('Properties')
+spherePropertiesFolder.add(sphereData, 'radius', 0.1, 30).onChange(regenerateSphereGeometry)
+spherePropertiesFolder.add(sphereData, 'widthSegments', 1, 32).onChange(regenerateSphereGeometry)
+spherePropertiesFolder.add(sphereData, 'heightSegments', 1, 16).onChange(regenerateSphereGeometry)
+spherePropertiesFolder
+    .add(sphereData, 'phiStart', 0, Math.PI * 2)
+    .onChange(regenerateSphereGeometry)
+spherePropertiesFolder
+    .add(sphereData, 'phiLength', 0, Math.PI * 2)
+    .onChange(regenerateSphereGeometry)
+spherePropertiesFolder.add(sphereData, 'thetaStart', 0, Math.PI).onChange(regenerateSphereGeometry)
+spherePropertiesFolder.add(sphereData, 'thetaLength', 0, Math.PI).onChange(regenerateSphereGeometry)
 
+const camFolder = gui.addFolder('Camera')
+const camPropertiesFolder = camFolder.addFolder('Properties')
+camPropertiesFolder.add(camera, 'fov', 0, 360)
+
+function regenerateSphereGeometry() {
+    const newGeometry = new THREE.SphereGeometry(
+        sphereData.radius,
+        sphereData.widthSegments,
+        sphereData.heightSegments,
+        sphereData.phiStart,
+        sphereData.phiLength,
+        sphereData.thetaStart,
+        sphereData.thetaLength
+    )
+    sphere.geometry.dispose()
+    sphere.geometry = newGeometry
+}
+const debug = document.getElementById('debug1') as HTMLDivElement
+const clock = new THREE.Clock()
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    // cube.rotation.y += 0.01
-    controls.update()
+
+    // scene.rotateY(0.0001)
+    // scene.rotateX(0.001)
+    
+    // let newPosition:THREE.Vector= path.getPoint((time % 2000)/2000)
+    // let pos:THREE.Vector3 = new THREE.Vector3(
+    //     newPosition.getComponent(0),
+    //     newPosition.getComponent(1),
+    //     newPosition.getComponent(2)
+    // );
+    // sphere.position.copy(pos)
+    sphere.position.x = 4+Math.sin(time/1000);
+    sphere.position.z = 4+Math.sin(time/1000);
+    // sphereData.heightSegments = Math.floor(3 + Math.abs(30*Math.sin(time/3000)))
+    // sphereData.widthSegments = Math.floor(3 + Math.abs(30*Math.cos(time/3000)))
+    sphereData.thetaLength = time/2000
+    regenerateSphereGeometry()
     render()
+
+    camera.fov = 60 + 50*Math.sin(time/10000);
+
+    camera.updateProjectionMatrix()
+
+    //debug.innerText = 'Matrix\n' + cube.matrix.elements.toString().replace(/,/g, '\n')
+
     stats.update()
 }
 
